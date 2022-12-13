05a43880c39411fac18299f229f7598971b3ade9
A. Ichsan Mudatsir <iksan0049@gmail.com>
2022-12-13 16:16:04 +0800

    Membuat To Do App Final Project

---
+++ app/src/main/java/com/D121201009/myaplikasitask/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.D121201009.myaplikasitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
